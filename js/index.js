605da0bbdf9318fd6cd8fb2602e1f742c95f0bc8
chandan1602 <cbansal82@gmail.com>
2019-06-16 20:50:04 +0530

    The feature of connecting the Pages to the index page has been setup

--- js/index.js
+++ js/index.js
@@ -1,3 +1,182 @@
-function gotoPage(select) {
-    (select.selectedIndex === 1) ? (window.location = select.value) : (alert(select.options[select.selectedIndex].value))
+function gotoPage(oForm) {
+    var college = oForm.elements["pumateCollegeSelect"].options.selectedIndex;
+    var stream = oForm.elements["pumateStreamSelect"].options.selectedIndex;
+    var semester = oForm.elements["pumateSemesterSelect"].options.selectedIndex;
+
+        if (college === 1 && stream === 1 && semester === 1) {
+            window.location = "F:/My Websites/IPD/html/cse/cse_sem1.html";
+            return false;
+        }
+
+        if(college===1 && stream===1 && semester===2) {
+            window.location = "F:/My Websites/IPD/html/cse/cse_sem2.html";
+            return false;
+        } 
+
+        if (college === 1 && stream === 1 && semester === 3) {
+            window.location = "F:/My Websites/IPD/html/cse/cse_sem3.html";
+            return false;
+        }
+
+        if (college === 1 && stream === 1 && semester === 4) {
+            window.location = "F:/My Websites/IPD/html/cse/cse_sem4.html";
+            return false;
+        }
+
+        if (college === 1 && stream === 1 && semester === 5) {
+            window.location = "F:/My Websites/IPD/html/cse/cse_sem5.html";
+            return false;
+        }
+
+        if (college === 1 && stream === 1 && semester === 6) {
+            window.location = "F:/My Websites/IPD/html/cse/cse_sem6.html";
+            return false;
+        }
+
+        if (college === 1 && stream === 1 && semester === 7) {
+            window.location = "F:/My Websites/IPD/html/cse/cse_sem7.html";
+            return false;
+        }
+
+        if (college === 1 && stream === 1 && semester === 8) {
+            window.location = "F:/My Websites/IPD/html/cse/cse_sem8.html";
+            return false;
+        }       
+
+        
+        
+                if (college === 1 && stream === 4 && semester === 1) {
+                    window.location = "F:/My Websites/IPD/html/it/it_sem1.html";
+                    return false;
+                }
+
+                if (college === 1 && stream === 4 && semester === 2) {
+                    window.location = "F:/My Websites/IPD/html/it/it_sem2.html";
+                    return false;
+                }
+
+                if (college === 1 && stream === 4 && semester === 3) {
+                    window.location = "F:/My Websites/IPD/html/it/it_sem3.html";
+                    return false;
+                }
+
+                if (college === 1 && stream === 4 && semester === 4) {
+                    window.location = "F:/My Websites/IPD/html/it/it_sem4.html";
+                    return false;
+                }
+
+                if (college === 1 && stream === 4 && semester === 5) {
+                    window.location = "F:/My Websites/IPD/html/it/it_sem5.html";
+                    return false;
+                }
+
+                if (college === 1 && stream === 4 && semester === 6) {
+                    window.location = "F:/My Websites/IPD/html/it/it_sem6.html";
+                    return false;
+                }
+
+                if (college === 1 && stream === 4 && semester === 7) {
+                    window.location = "F:/My Websites/IPD/html/it/it_sem7.html";
+                    return false;
+                }
+
+                if (college === 1 && stream === 4 && semester === 8) {
+                    window.location = "F:/My Websites/IPD/html/it/it_sem8.html";
+                    return false;
+                }
+
+
+                                if (college === 1 && stream === 2 && semester === 1) {
+                                    window.location = "F:/My Websites/IPD/html/ece/ece_sem1.html";
+                                    return false;
+                                }
+
+                                if (college === 1 && stream === 2 && semester === 2) {
+                                    window.location = "F:/My Websites/IPD/html/ece/ece_sem2.html";
+                                    return false;
+                                }
+
+                                if (college === 1 && stream === 2 && semester === 3) {
+                                    return false;
+                                    window.location = "F:/My Websites/IPD/html/ece/ece_sem3.html";
+                                }
+
+                                if (college === 1 && stream === 2 && semester === 4) {
+                                    window.location = "F:/My Websites/IPD/html/ece/ece_sem4.html";
+                                    return false;
+                                }
+
+                                if (college === 1 && stream === 2 && semester === 5) {
+                                    window.location = "F:/My Websites/IPD/html/ece/ece_sem5.html";
+                                    return false;
+                                }
+
+                                if (college === 1 && stream === 2 && semester === 6) {
+                                    window.location = "F:/My Websites/IPD/html/ece/ece_sem6.html";
+                                    return false;
+                                }
+
+                                if (college === 1 && stream === 2 && semester === 7) {
+                                    window.location = "F:/My Websites/IPD/html/ece/ece_sem7.html";
+                                    return false;
+                                }
+
+                                if (college === 1 && stream === 2 && semester === 8) {
+                                    window.location = "F:/My Websites/IPD/html/ece/ece_sem8.html";
+                                    return false;
+                                }
+
+                                                if (college === 1 && stream === 3 && semester === 1) {
+                                                    window.location = "F:/My Websites/IPD/html/mech/mech_sem1.html";
+                                                    return false;
+                                                }
+
+                                                if (college === 1 && stream === 3 && semester === 2) {
+                                                    window.location = "F:/My Websites/IPD/html/mech/mech_sem2.html";
+                                                    return false;
+                                                }
+
+                                                if (college === 1 && stream === 3 && semester === 3) {
+                                                    window.location = "F:/My Websites/IPD/html/mech/mech_sem3.html";
+                                                    return false;
+                                                }
+
+                                                if (college === 1 && stream === 3 && semester === 4) {
+                                                    window.location = "F:/My Websites/IPD/html/mech/mech_sem4.html";
+                                                    return false;
+                                                }
+
+                                                if (college === 1 && stream === 3 && semester === 5) {
+                                                    window.location = "F:/My Websites/IPD/html/mech/mech_sem5.html";
+                                                    return false;
+                                                }
+
+                                                if (college === 1 && stream === 3 && semester === 6) {
+                                                    window.location = "F:/My Websites/IPD/html/mech/mech_sem6.html";
+                                                    return false;
+                                                }
+
+                                                if (college === 1 && stream === 3 && semester === 7) {
+                                                    window.location = "F:/My Websites/IPD/html/mech/mech_sem7.html";
+                                                    return false;
+                                                }
+
+                                                if (college === 1 && stream === 3 && semester === 8) {
+                                                    window.location = "F:/My Websites/IPD/html/mech/mech_sem8.html";
+                                                    return false;
+                                                }
+
+
+    else {
+        alert('This Selection is not Available! We are Working on it!!');
+    }
+}
+
+    // (select.selectedIndex === 1) ? (window.location = "html/pussgrc.html") : (alert(select.options[select.selectedIndex].value))
+// }
+
+function gotoPageSelect(select) {
+    if(select.selectedIndex !== 1) {
+        alert(select.options[select.selectedIndex].value);
+    }
 }
