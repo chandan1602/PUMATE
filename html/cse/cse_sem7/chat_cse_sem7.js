617a5525e2f12c74b13dace09e002445c64bd6f9
chandan1602 <cbansal82@gmail.com>
2019-06-24 04:05:17 +0530

    All linking of cse_ done except gallery

---
+++ html/cse/cse_sem7/chat_cse_sem7.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/chat_cse_sem7.html');
+});
+
+// It displays the message on console.
+// io.on('connection', function(socket){
+//     socket.on('chat message', function(msg){
+//       console.log('message: ' + msg);
+//     });
+//   });  
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+  });
+
+
+http.listen(4000, function(){
+  console.log('listening on *:4000');
+});
